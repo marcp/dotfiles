e3b31e0b95f7da920ad4dab11d35e1b74c936fa3
Marc Poris <mporis@airadvice.com>
2013-04-30 12:14:52 -0700

    Cleaning out stuff I don't use yet.

--- .lein/profiles.clj
+++
@@ -1,2 +0,0 @@
-{:user {:plugins [[lein-tarsier "0.9.1"]
-                  [jonase/kibit "0.0.4"]]}}
--- install.sh
+++ install.sh
@@ -2,27 +2,15 @@
 
 PWD="$( cd "$( dirname "$0" )" && pwd )"
 
-ln -nfs $PWD/.colors $HOME/.colors
-ln -nfs $PWD/.cvsignore $HOME/.cvsignore
 ln -nfs $PWD/.dircolors $HOME/.dircolors
 ln -nfs $PWD/.fonts/* $HOME/.fonts
-ln -nfs $PWD/.ghci $HOME/.ghci
 ln -nfs $PWD/.gitconfig $HOME/.gitconfig
-ln -nfs $PWD/.hgrc $HOME/.hgrc
+ln -nfs $PWD/.gitignore_global $HOME/.gitignore_global
 ln -nfs $PWD/.inputrc $HOME/.inputrc
-ln -nfs $PWD/.jshintrc $HOME/.jshintrc
-ln -nfs $PWD/.lein/*.clj $HOME/.lein
-ln -nfs $PWD/.ncmpcpp $HOME/.ncmpcpp
-ln -nfs $PWD/.pentadactylrc $HOME/.pentadactylrc
-ln -nfs $PWD/.scheme_completion $HOME/.scheme_completion
 ln -nfs $PWD/.tmux.conf $HOME/.tmux.conf
 ln -nfs $PWD/.vim $HOME/.vim
 ln -nfs $PWD/.vimrc $HOME/.vimrc
 ln -nfs $PWD/.xinitrc $HOME/.xinitrc
-ln -nfs $PWD/.xmobarrc $HOME/.xmobarrc
-ln -nfs $PWD/.xmonad $HOME/.xmonad
-ln -nfs $PWD/.Xresources $HOME/.Xresources
-ln -nfs $PWD/.xswap $HOME/.xswap
 ln -nfs $PWD/.zlogin $HOME/.zlogin
 ln -nfs $PWD/.zlogout $HOME/.zlogout
 ln -nfs $PWD/.zprezto $HOME/.zprezto
